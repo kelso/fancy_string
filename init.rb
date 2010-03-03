94a859c45ca7cea2e2c17d6a02ae172163b6a7e5
Stefan Huska <stefan.huska@smartmedia.sk>
2010-03-03 22:35:40 +0100

    initial commit

---
+++ init.rb
@@ -0,0 +1,13 @@
+String.class_eval do
+  def fancy
+    str = self
+    str = str.gsub(":"," ")
+    str = str.gsub("-"," ")
+    str = str.gsub(/[ ]+/," ") # replace "multi" space to one
+    str = str.gsub(/ /,"-")    # replace each space to -
+    str = str.downcase
+    str = str.mb_chars.normalize(:kd).gsub(/[^\-x00-\x7F.]/n, '').to_s
+    return str
+  end
+end
+
